6aec39b1480f96acc6a506d163b607a8daf9789c
n7484443 <n7484443@naver.com>
2023-04-20 13:39:01 +0900

    Refactor sample

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,9 +13,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List data = [0, 1, 2, 3, [0, 1, 2, 3, [0, 1, 2, 3, 4], 4, 5, 6]];
+
   @override
   void initState() {
     super.initState();
+  }
+
+  Widget createNested(List<int> currentPos, List dataInput){
+    return TestUnit(
+      color: Colors.indigoAccent,
+      pos: currentPos,
+      itemBuilder: (context, index) {
+        var pos = [...currentPos, index];
+        if(dataInput[index] is List){
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            key: Key('$pos test $index'),
+            child: createNested(pos, dataInput[index]),
+          );
+        }
+        return Card(
+          key: Key('$pos test $index'),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('$pos test ${dataInput[index]}'),
+          ),
+        );
+      },
+      itemCount: dataInput.length,
+    );
   }
 
   @override
@@ -26,60 +53,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: MultiLevelDraggableParent(
-          child: TestUnit(
-            pos: const [0],
-            color: Colors.indigo,
-            itemBuilder: (context, index) {
-              if (index == 4) {
-                return Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  key: Key('${[0, 0]} test $index'),
-                  child: TestUnit(
-                    color: Colors.indigoAccent,
-                    pos: const [0, 0],
-                    itemBuilder: (context, index) {
-                      if (index == 4) {
-                        return Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          key: Key('${[0, 0, 0]} test $index'),
-                          child: TestUnit(
-                            color: Colors.indigoAccent,
-                            pos: const [0, 0, 0],
-                            itemBuilder: (context, index) {
-                              return Card(
-                                key: Key('${[0, 0, 0]} test $index'),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text('test $index'),
-                                ),
-                              );
-                            },
-                            itemCount: 5,
-                          ),
-                        );
-                      }
-                      return Card(
-                        key: Key('${[0, 0]} test $index'),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text('test $index'),
-                        ),
-                      );
-                    },
-                    itemCount: 5,
-                  ),
-                );
-              }
-              return Card(
-                key: Key('${[0]} test $index'),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('test $index'),
-                ),
-              );
-            },
-            itemCount: 5,
-          ),
+          child: createNested([], data),
         ),
       ),
     );
